60b04c7cbd7b72f198ecb584356ebda109933687
Audrey WL <audrey.lewis@students.olin.edu>
2017-12-10 22:06:59 -0500

    works on raspi

--- find_basket.py
+++ find_basket.py
@@ -2,29 +2,38 @@ import numpy as np
 import cv2
 import cv2.aruco as aruco
 import time
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+ 
+
+
 
 class Detector(object):
     """wrapper to hold all the image processing, contour finding operations"""
     def __init__(self, camera):
-        self.camera = camera
-        self.cap = cv2.VideoCapture(self.camera)
+	# initialize the camera and grab a reference to the raw camera capture
+	self.camera = PiCamera()
+	self.camera.resolution = (640, 480)
+	self.camera.framerate = 32
+	self.cap = PiRGBArray(camera, size=(640, 480))
+	# allow the camera to warmup
+	time.sleep(0.1)
         self.dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
         self.ar_params = aruco.DetectorParameters_create()
         #self.test_marker = aruco.drawMarker(self.dict, 23, 700)
 
-    def update(self):
-    	# Capture frame-by-frame
-        ret, frame = self.cap.read()
-
-
-        # transform image to grayscale and blur
+    def update(self, frame):
+        # transform image to grayscale
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, self.dict, parameters=self.ar_params)
 
-        aruco.estimatePoseSingleMarkers(corners, markerLength, cameraMatrix, distCoeffs)
+        #aruco.estimatePoseSingleMarkers(corners, markerLength, cameraMatrix, distCoeffs)
 
         self.debug_img = aruco.drawDetectedMarkers(gray, corners, ids)
+	
+	# clear the stream in preparation for the next frame
+	self.cap.truncate(0)
 
 
     def kill_video(self):
@@ -34,10 +43,10 @@ class Detector(object):
 
 if __name__ == '__main__':
     prime_detector = Detector(0);
-    #cv2.imshow('debug', prime_detector.test_marker)
-    running = True
-    while  running:
-        prime_detector.update()
+    for frame in prime_detector.camera.capture_continuous(prime_detector.cap, format="bgr", use_video_port=True):
+	image = frame.array
+        prime_detector.update(image)
         cv2.imshow('debug',prime_detector.debug_img)
         time.sleep(.03)
         cv2.waitKey(30)
+
